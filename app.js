333716cb231699e767a6b39585a3f3c832a1ae20
Jarom Oyler <bubba@Bubba.local>
2022-12-19 09:45:22 -0700

    Adding Form

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const fs = require("fs")
 const app = express();
 
 const port = process.env.PORT || 3400 ;
+app.use(express.urlencoded({extended:false}))
 app.use(express.static("public"))
 
 app.get("/about", (req, res)=>{
     res.send("Hello World")
 })
 
+app.post("/", (req,res)=>{
+    const color = req.body.favcolor
+    const first = req.body.firstname
+    const last = req.body.lastname
 
+    console.log("New User Been add: "+first+" "+" "+last+" "+color)
+    res.send("<h1 class='form'>New User Been add: "+first+" "+" "+last+", "+color+"<h1>")
+})
 
 
 
