316109efd2a414f0464446cda8fea507d782e1db
Jarom Oyler <bubba@Bubba.local>
2022-12-19 09:13:20 -0700

    add Image

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express")
-
+const fs = require("fs")
 const app = express();
 
 const port = process.env.PORT || 3400 ;
+app.use(express.static("public"))
 
 app.get("/about", (req, res)=>{
     res.send("Hello World")
 })
 
+
+
+
+
 app.listen(port, () => {
     console.log("Listening on Port: "+ port )
 })
+
+
+
+
+
