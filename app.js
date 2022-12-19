2ac6b83cba74bf7067a78ac2286b39f5cc550950
Jarom Oyler <bubba@Bubba.local>
2022-12-19 08:21:21 -0700

    Im Your Daddy

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express();
+
+const port = process.env.Port || 3400 ;
+
+app.get("/about", (req, res)=>{
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log("Listening on Port: "+ port )
+})
