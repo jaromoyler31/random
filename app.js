6cca1e5b7c2606fc96511773fdf72cc14abaecb1
Jarom Oyler <bubba@Bubba.local>
2022-12-19 08:56:14 -0700

    Typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const app = express();
 
-const port = process.env.Port || 3400 ;
+const port = process.env.PORT || 3400 ;
 
 app.get("/about", (req, res)=>{
     res.send("Hello World")
